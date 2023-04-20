298c9d11c6aeef1beb95fa20a49d214ae61e180d
hottnerd <111224077+hottnerd@users.noreply.github.com>
2023-04-20 14:25:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session({
     saveUninitialized: false,
     cookie:{
         maxAge: 1000 * 60 * 60 ,
-        sameSite : "none",
+        sameSite : "strict",
         secure : true,
     },
     rolling:true,
